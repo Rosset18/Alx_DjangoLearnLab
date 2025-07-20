fcda66b4805f369e858b489e1f525412d924f05c
Rosset18 <makhado.phindulo@outlook.com>
2025-07-20 11:21:36 +0200

    updated the urls

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -2,7 +2,7 @@
 URL configuration for LibraryProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,28 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView, TemplateView
-from .views import list_books, LibraryDetailView, index, add_book, change_book, delete_book
-from . import views
-from .views import librarian_view, member_view
-from .admin_view import admin_view
-
 
 urlpatterns = [
-    path('books/', list_books, name='list_all_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_books'),
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('', index, name='index'),
-    path('accounts/profile/', TemplateView.as_view(template_name='relationship_app/profile.html'), name='profile'),
-
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian_view/', librarian_view, name='librarian_view'),
-    path('member_view/', member_view, name='member_view'),
-
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:book_id>/', change_book, name='change_book'),
-    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
+    path('admin/', admin.site.urls),
 ]
