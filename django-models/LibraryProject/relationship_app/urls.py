43d293f19f28281e3d6787c7cc4e6c880e2e15c9
Rosset18 <makhado.phindulo@outlook.com>
2025-07-20 12:14:25 +0200

    changes to url

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,56 +1,25 @@
 from django.urls import path
-from .admin_view import admin_view
-from .librarian_view import librarian_view
-from .member_view import member_view
-import views
-from .views import list_books, LibraryDetailView, register
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import add_book, edit_book, delete_book
-
-urlpatterns = [
-    path('books/', list_books, name='book-list'),
-    path('library/', LibraryDetailView.as_view(), name = "library-detail"),
-    path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name = 'login'),
-    path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name = 'logout'),
-    path('register/', views.register.as_view(), name = "register"),
-    path('admin/', admin_view, name='admin-view'),
-    path('librarian/', librarian_view, name='librarian-view'),
-    path('member/', member_view, name='member-view'),
-    path('add_book/', add_book, name='add-book'),
-    path('edit_book/', edit_book, name='edit-book'),
-    path('delete_book/', delete_book, name='delete-book'),
-]
-
-'''from django.urls import path
-from .views import list_books, LibraryDetailView
-
-urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
-
-
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, TemplateView
+from .views import list_books, LibraryDetailView, index, add_book, change_book, delete_book
 from . import views
+from .views import librarian_view, member_view
+from .admin_view import admin_view
+
 
 urlpatterns = [
+    path('books/', list_books, name='list_all_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_books'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
-    
-    # Add other URL patterns here
+    path('', index, name='index'),
+    path('accounts/profile/', TemplateView.as_view(template_name='relationship_app/profile.html'), name='profile'),
+
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
+    path('member_view/', member_view, name='member_view'),
+
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', change_book, name='change_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
-
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add_book/', views.add_book, name='add_book'),
-    path('bedit_book/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
-    
-    # Other URL patterns...
-]
-'''
